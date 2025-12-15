5292cc0674465ee8c83dd383f629b00868d5cf4c
rubenbarr <benjybeltran1393@gmail.com>
2025-12-15 15:23:32 -0600

    new changesss

--- src/app/changePassword/page.tsx
+++ src/app/changePassword/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useEffect, useReducer, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useAuth } from "@/context/AuthContext";
